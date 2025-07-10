c5432f176f28b90051d3db76bfae9a4db3f5c38e
Amaury Pouly <amaury.pouly@lowrisc.org>
2025-07-10 16:32:02 +0200

    check_picks: add support for Google spreadsheet
    Signed-off-by: Amaury Pouly <amaury.pouly@lowrisc.org>

--- cherrypicks/check_picks.py
+++ cherrypicks/check_picks.py
@@ -14,11 +14,34 @@ import sqlite3
 import html
 from pprint import pprint
 from copy import copy
+from pathlib import Path
+from dataclasses import dataclass
+import json
 
-GREEN = "#93c47d"
-LGREEN = "#b6d7a8"
-NA = "#c0c4c9"
-YELLOW = "#ffff00"
+COLORS_HTML = {
+    "green": "#93c47d",
+    "lgreen": "#b6d7a8",
+    "n/a": "#c0c4c9",
+    "yellow": "#ffff00",
+}
+
+COLORS_GSPREAD = {
+    "green": {
+        "red": 0x93 / 256,
+        "green": 0xc4 / 256,
+        "blue": 0x7d / 256,
+    },
+    "n/a": {
+        "red": 0xc0 / 256,
+        "green": 0xc4 / 256,
+        "blue": 0xc9 / 256,
+    },
+    "yellow": {
+        "red": 0xff / 256,
+        "green": 0xff / 256,
+        "blue": 0,
+    }
+}
 
 MANUAL = {
     24834: {"pick": 24345, "from": "master"},
@@ -46,6 +69,10 @@ flags.add_argument(
 flags.add_argument("--gh_bin", default="gh", help="Github CLI binary")
 flags.add_argument("-b", "--branch", type=str, help="Branch to check")
 flags.add_argument("database", type=str, help="Database file")
+flags.add_argument("--html", type=Path, help="Output HTML table to a file")
+flags.add_argument("--spreadsheet-url", type=str, help="Update Google spreadsheet at the given URL")
+flags.add_argument("--credentials", type=Path, help="""Path to a json file with the google credentials.
+Check https://docs.gspread.org/en/latest/oauth2.html for more details.""",)
 
 
 class CommitDatabase(object):
@@ -170,25 +197,52 @@ def tabulate_branch(prs, branch):
     return table
 
 
-def render_pr_link(pr):
+class Cell:
+    def render_html(self): pass
+    def render_gspread(self): pass
+
+@dataclass
+class UrlCell(Cell):
+    url: str
+    label: str
+
+    def render_html(self):
+        return '<a href="{}">{}</a>'.format(self.url, self.label)
+
+    def render_gspread(self):
+        return '=HYPERLINK("{}", "{}")'.format(self.url, self.label)
+
+def render_pr_link(pr, label = None):
+    if label is None:
+        label = f"{pr}"
     if pr:
-        return f'<a href="https://github.com/lowRISC/opentitan/pull/{pr}">{pr}</a>'
+        return UrlCell(url = f'https://github.com/lowRISC/opentitan/pull/{pr}', label = label)
     else:
         return pr
 
 
 def render_table(table):
-    data = []
+    """
+    Render the table. The output is a dictionary with the following keys:
+    - headers: list of strings
+    - rows: list of dictionaries (see below)
+    - desc: list of strings (free-style description)
 
-    print('<table border="1">')
-    print("<tr>")
-    print("<th>PR</th>")
-    print("<th>To Master</th>")
-    print("<th>From</th>")
-    print("<th>Title</th>")
-    print("<th>Merged</th>")
-    print("<th>Author</th>")
-    print("</tr>")
+    Each row is described by a dictionary:
+    - color: name of the color
+    - columns: list of cells (see below)
+
+    Each cell must either be a string, or an object inheriting Cell.
+    """
+    headers = [
+        "PR",
+        "To Master",
+        "From",
+        "Title",
+        "Merged",
+        "Author",
+    ]
+    rows = []
 
     picked = 0
     unpicked = 0
@@ -201,7 +255,7 @@ def render_table(table):
             pick = ""
         to_master = v.get("to_master", "")
         source = v["from"]
-        notes = ""
+        notes = v.get("notes", "")
 
         if manual := MANUAL.get(k):
             # Manual overrides for this PR?
@@ -210,56 +264,173 @@ def render_table(table):
             source = manual.get("from", source)
             notes = manual.get("notes", "")
 
+        color = None
         if to_master or source == "master":
-            rowstyle = f'bgcolor="{GREEN}"'
+            color = "green"
             picked += 1
         else:
             rowstyle = ""
             unpicked += 1
 
         if source == "N/A":
-            rowstyle = f'bgcolor="{NA}"'
+            color = "n/a"
 
         if notes:
-            rowstyle = f'bgcolor="{YELLOW}"'
+            color = color or "yellow"
             notes = " <b>(NOTE: {})</b>".format(html.escape(notes))
 
-        print(f"<tr {rowstyle}>")
-        print("<td>{}</td>".format(render_pr_link(k)))
+        columns = [
+            render_pr_link(k),
+        ]
         if to_master:
-            print("<td>{}</td>".format(render_pr_link(to_master)))
+            columns.append(render_pr_link(to_master))
         else:
-            print("<td></td>")
+            columns.append("")
         if pick:
-            print("<td>{} on {}</td>".format(render_pr_link(pick), source))
+            columns.append(render_pr_link(pick, f"{pick} on {source}"))
         else:
-            print("<td></td>")
-        print(
-            "<td>{}{}</td>".format(
-                html.escape(v["title"]),
-                notes,
-            )
-        )
-        print("<td>{}</td>".format(html.escape(v["mergedAt"])))
-        print(
-            "<td>{}:{}</td>".format(
+            columns.append("")
+        columns.append("{}{}".format(
+            html.escape(v["title"]),
+            notes,
+        ))
+        columns.append("{}".format(html.escape(v["mergedAt"])))
+        columns.append(
+            "{}:{}".format(
                 html.escape(v["headRepositoryOwner"]["login"]),
                 html.escape(v["headRefName"]),
             )
         )
-        print("</tr>")
-    print("</table>")
-    print(f"<p>{picked} of {total} already exist on master</p>")
-    print(f"<p>{unpicked} of {total} need cherry-picks</p>")
+        rows.append({
+            "color": color,
+            "columns": columns,
+        })
+
+    return {
+        "headers": headers,
+        "rows": rows,
+        "desc": [
+            f"{picked} of {total} already exist on master",
+            f"{unpicked} of {total} need cherry-picks"
+        ]
+    }
+
+
+def render_html(html_path, table):
+    data = []
+
+    html_out = ""
+    def fprint(s):
+        nonlocal html_out
+        html_out += s
+
+    fprint('<table border="1">')
+    fprint("<tr>")
+    for hdr in table["headers"]:
+        fprint(f"<th>{hdr}</th>")
+    fprint("</tr>")
+
+    for row in table["rows"]:
+        rowstyle = 'bgcolor="{}"'.format(COLORS_HTML[row["color"]]) if row["color"] else ""
+
+        fprint(f"<tr {rowstyle}>")
+        for cell in row["columns"]:
+            if isinstance(cell, Cell):
+                fprint("<td>{}</td>".format(cell.render_html()))
+            else:
+                fprint(f"<td>{cell}</td>")
+        fprint("</tr>")
+    fprint("</table>")
+    for desc in table["desc"]:
+        fprint(f"<p>{desc}</p>")
+
+    html_path.write_text(html_out)
+
+
+def open_gspread(args):
+    import gspread
+    if not args.credentials:
+        sys.exit("You need to pass credentials using --credentials")
+    gc = gspread.oauth(credentials_filename=args.credentials)
+    return gc.open_by_url(args.spreadsheet_url)
+
+def render_gspread(spreadsheet, table):
+    sheet = spreadsheet.worksheet("Status")
+    print("Rendering to gspread...")
+
+    # The first row is for cookies, the second row for warning text, third row is empty
+    first_table_row = 4
+
+    # We store a special value in A1 to detect if the spreadsheet was updated by
+    # this script, and in A2 we stored a json table. We always hide the first row
+    # to hide this.
+    MAGIC_COOKIE = "ot-script cookie"
+    if sheet.get("A1") == [[MAGIC_COOKIE]]:
+        cookie = json.loads(sheet.get("B1")[0][0])
+    else:
+        cookie = {
+            "first_table_row": first_table_row
+        }
+
+    if cookie["first_table_row"] != first_table_row:
+        sys.exit("Error: the spreadhseet current uses first_table_row={} but this script wants {}.\n".format(cookie["first_table_row"], first_table_row) +
+                 "The script will not update automatically to avoid errors, you must manually update the spreadsheet")
+
+    sheet.update_acell("A1", MAGIC_COOKIE)
+    sheet.update_acell("B1", json.dumps(cookie))
+    sheet.hide_rows(0, 1)
+
+    sheet.update_acell("A2", "This spreadsheet is automatically generated by the ot-script, you can edit columns starting from {}".format(
+                       chr(ord('A')+len(table["headers"]))))
+    sheet.format("A2", {'textFormat': {'bold': True}})
+
+    data_range = 'A{}:{}{}'.format(first_table_row, chr(ord('A')+len(table["headers"])-1), first_table_row + len(table["rows"]))
+
+    sheet.update([table["headers"]] + [
+        [
+            cell.render_gspread() if isinstance(cell, Cell) else cell
+            for cell in row["columns"]
+        ]
+        for row in table["rows"]
+    ], data_range, raw = False)
+    # Add colors
+    sheet.batch_format([
+        {
+            "range": '{}'.format(first_table_row + i),
+            "format": {
+                "backgroundColor": COLORS_GSPREAD[row["color"]]
+            },
+        }
+        for (i, row) in enumerate(table["rows"]) if row["color"] is not None
+    ])
+    # Make headers bold and frozen
+    sheet.format(str(first_table_row), {'textFormat': {'bold': True}})
+    sheet.freeze(first_table_row)
+    # Protect autogenerated data: first remove old ranges and then create new ones
+    PROTECTED_RANGE_DESC = "Auto-generated content"
+    for rng in spreadsheet.list_protected_ranges(sheet.id):
+        # Content does not seem to be documented?
+        if rng["description"] == PROTECTED_RANGE_DESC:
+            sheet.delete_protected_range(rng["protectedRangeId"])
+
+    sheet.add_protected_range(
+        data_range,
+        description = PROTECTED_RANGE_DESC,
+    )
 
 
 def main(args):
 
     db = CommitDatabase(args.database)
     prs = db.get_prs()
-    index_cherrypicks(prs)
+    index_cherrypicks(db, prs)
     table = tabulate_branch(prs, args.branch)
-    render_table(table)
+    table = render_table(table)
+    if args.html:
+        render_html(args.html, table)
+    if args.spreadsheet_url:
+        spreadsheet = open_gspread(args)
+        render_gspread(spreadsheet, table)
     return 0
 
 
